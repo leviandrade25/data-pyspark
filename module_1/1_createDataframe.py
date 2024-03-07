b7d206a72b1b28f437365a8d2e8d75cac292d4d7
Levi de Andrade landrade <landrade@applaudostudios.com>
2024-03-07 14:22:30 -0300

    sript 1

---
+++ module_1/1_createDataframe.py
@@ -0,0 +1,12 @@
+"""
+# Exemplo de Script Python com Markdown
+
+Este script exemplifica como incluir **Markdown** em um script Python.
+
+## Função exemplo
+
+Abaixo está uma função que soma dois números:
+
+"""
+def soma(a, b):
+    return a + b
